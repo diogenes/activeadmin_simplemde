0900f55dea1ef4e16cff80c7b50b69f2336509c4
Diógenes Falcão <diogenes.araujo@gmail.com>
2020-02-02 20:42:19 -0300

    Small config fix

--- app/assets/javascripts/activeadmin/simplemde_editor_input.js
+++ app/assets/javascripts/activeadmin/simplemde_editor_input.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     $('.simplemde-editor').each(function () {
         var options = {
+	  element: $(this).get(0)
 	};
         options = $.extend({}, options, $(this).data('options'));
-        new SimpleMDE( { 
-			element: $(this).get(0), 
-			options: options,
-		});
+        new SimpleMDE(options);
     });
 });
