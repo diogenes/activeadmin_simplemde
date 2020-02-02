e2430ed6188a5913aa61c38711e5c6868ffed589
Diógenes Falcão <diogenes.araujo@gmail.com>
2020-02-02 20:35:46 -0300

    Revert wrong spell checker configuration

--- app/assets/javascripts/activeadmin/simplemde_editor_input.js
+++ app/assets/javascripts/activeadmin/simplemde_editor_input.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     $('.simplemde-editor').each(function () {
         var options = {
-	  spellChecker: false
 	};
         options = $.extend({}, options, $(this).data('options'));
         new SimpleMDE( { 
 			element: $(this).get(0), 
-			options: options 
+			options: options,
 		});
     });
 });
